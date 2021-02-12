9828ba350e85c6a21634bb010fc5f938c8aa0a42
neoncloud <neoncloud2119@gmail.com>
2021-02-12 23:28:56 +0800

    improve eval() and train()

--- train.py
+++ train.py
@@ -7,6 +7,8 @@ import torchvision.datasets as dsets
 from torch.utils.data import Dataset, DataLoader
 from pathlib import Path
 import os
+import datetime
+from tqdm import tqdm
 
 import torch.nn.functional as F
 import torch.nn as NN
@@ -17,9 +19,12 @@ TRAIN_DATA_PATH = Path('./data/train')
 BATCH_SIZE = 64
 
 model = torchvision.models.resnet18(pretrained = False)
+model.fc = NN.Linear(512, 5)
+print(model)
 model = model.cuda()
 transform = transforms.Compose([
-    transforms.RandomCrop(32, padding = 4, pad_if_needed = True),  #先四周填充0，在把图像随机裁剪成32*32
+    transforms.ColorJitter(brightness=0.5),
+    transforms.RandomCrop(128, padding = 4, pad_if_needed = True, padding_mode='symmetric'),
     transforms.RandomHorizontalFlip(),  #图像一半的概率翻转，一半的概率不翻转
     transforms.RandomRotation((-45, 45)), #随机旋转
     transforms.ToTensor(),
@@ -29,35 +34,43 @@ transform = transforms.Compose([
 eval_dataset = dsets.ImageFolder(EVAL_DATA_PATH,transform = transform)
 train_dataset = dsets.ImageFolder(TRAIN_DATA_PATH,transform = transform)
 
-eval_dataloader = DataLoader(eval_dataset ,batch_size = BATCH_SIZE)
-train_dataloader = DataLoader(train_dataset ,batch_size = BATCH_SIZE)
+eval_dataloader = DataLoader(eval_dataset ,batch_size = BATCH_SIZE, shuffle=True)
+train_dataloader = DataLoader(train_dataset ,batch_size = BATCH_SIZE, shuffle=True)
 
 criterion = NN.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr = 0.001, momentum = 0.9)
 
-for epoch in range(10):  # 多批次循环
+for epoch in range(0, 100):
+    model.train()
+    with tqdm(train_dataloader, unit="batch") as tepoch:
+        for data, target in tepoch:
+            tepoch.set_description(f"Epoch {epoch}")
+            data, target = data.cuda(), target.cuda()
+            optimizer.zero_grad()
+            output = model(data)
+            predictions = output.argmax(dim=1, keepdim=True).squeeze()
+            loss = F.nll_loss(output, target)
+            correct = (predictions == target).sum().item()
+            accuracy = correct / BATCH_SIZE
+            
+            loss.backward()
+            optimizer.step()
 
-    running_loss = 0.0
-    for i, data in enumerate(train_dataloader, 0):
-        # 获取输入
-        inputs, labels = data
-        inputs = inputs.cuda()
-        labels = labels.cuda()
-        # 梯度置0
-        optimizer.zero_grad()
+            tepoch.set_postfix(loss=loss.item(), accuracy=100. * accuracy)
 
-        # 正向传播，反向传播，优化
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+    print("Epoch done, evaluating:", epoch)
+    model.eval()
+    with tqdm(eval_dataloader, unit="batch") as eepoch:
+        for data, target in eepoch:
+            eepoch.set_description(f"Epoch {epoch}")
+            data, target = data.cuda(), target.cuda()
+            output = model(data)
+            predictions = output.argmax(dim=1, keepdim=True).squeeze()
+            correct = (predictions == target).sum().item()
+            accuracy = correct / BATCH_SIZE
 
-        # 打印状态信息
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # 每2000批次打印一次
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
-            torch.save(model.state_dict(), ".")
+            eepoch.set_postfix(loss=loss.item(), accuracy=100. * accuracy)
+    if epoch % 10 == 0:
+        torch.save(model.state_dict(), "./" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".bin")
 
 print('Finished Training')
